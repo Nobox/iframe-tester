f5e74619069da0042fa9ffc4fdfd3c931b53162d
Waldemar Figueroa <waldemar@nobox.com>
2014-10-06 15:46:43 -0400

    Add code to set top offset on iframe modals

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,17 @@
-$('#nobox-iframe').iFrameResize({
-    log: true,
-    enablePublicMethods: true,
-    checkOrigin: false,
-    heightCalculationMethod: 'lowestElement'
+$(document).ready(function() {
+    // Handle iframe resizing
+    $('#nobox-iframe').iFrameResize({
+        log: true,
+        enablePublicMethods: true,
+        checkOrigin: false,
+        heightCalculationMethod: 'lowestElement'
+    });
+
+    // Send scrollTop value to iframe for modal positioning
+    iframe = document.getElementById('nobox-iframe');
+    $(window).on('scroll', function(e) {
+        window.requestAnimationFrame(function() {
+            iframe.contentWindow.postMessage($(window).scrollTop(), 'http://local-viajegenial.nbxapps.com:8000');
+        });
+    });
 });
